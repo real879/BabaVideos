a22c369999a31865cac1b19a4c0c72f280e3bc0a
Ren <renatto879@hotmail.com>
2020-05-04 00:24:49 -0500

    login-register

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider}from 'react-redux'
+import {createStore}from 'redux'
 import App from './routes/App';
-
-
 ReactDOM.render(
-    <App/>,
+    <Provider>
+        <App/>
+    </Provider>,
     document.getElementById('app')
 )
 
